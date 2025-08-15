8d4ce537eed0de520e95a7e439c271433d2722d3
Your Name <diwankhushi428@gmail.com>
2025-08-15 20:15:09 +0530

    step 2 adding placeholders

--- rust_day1/day1_practice/src/main.rs
+++ rust_day1/day1_practice/src/main.rs
@@ -1,11 +1,20 @@
-fn main() {
-    println!("Hello, khushi!"); //here println! is a macro, not a function (hence the !).
-}
+// fn main() {
+//     println!("Hello, khushi!"); //here println! is a macro, not a function (hence the !).
+// }
 
 // in rust a macro is code that writes code for u before the program is compiled
 // it takes input and generates rust code at compile time 
 // why is println! a macro? ...normal rust functions cant take “variable number of arguments” in the same flexible way (without extra boilerplate), but macros can
 
 
----
+//-------------------
+
+// step 2- adding placeholders
+
+fn main() {
+    let name = "ash";
+    println!("hello, {}!", name);
+}
+
+// {} is replaced in order with variables and if you add more {} than variables then it gives compile error
 

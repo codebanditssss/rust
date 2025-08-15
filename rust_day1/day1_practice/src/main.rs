b791f034e0752190ec0276d599d46ed86c670a9c
Your Name <diwankhushi428@gmail.com>
2025-08-15 20:17:52 +0530

    step 3 adding numbered placeholders

--- rust_day1/day1_practice/src/main.rs
+++ rust_day1/day1_practice/src/main.rs
@@ -11,10 +11,19 @@
 
 // step 2- adding placeholders
 
-fn main() {
-    let name = "ash";
-    println!("hello, {}!", name);
-}
+// fn main() {
+//     let name = "ash";
+//     println!("hello, {}!", name);
+// }
 
 // {} is replaced in order with variables and if you add more {} than variables then it gives compile error
 
+//----------------------
+
+//step 3- adding numbered placeholders
+
+fn main() {
+    println!("{0}, meet {1}. {1}, meet {0}.", "kiki", "ash");
+}
+
+// {0} and {1} let you reuse arguments without repeating

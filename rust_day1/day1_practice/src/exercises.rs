4b1ece7dfaa596f4222b7d95484097dda9a95c57
Your Name <diwankhushi428@gmail.com>
2025-08-16 00:23:29 +0530

    exercise 1 - mutable vs immutable

---
+++ rust_day1/day1_practice/src/exercises.rs
@@ -0,0 +1,10 @@
+//Exercise 1 — Mutable vs Immutable
+
+//Question:
+//Make a variable score = 10. Increase it by 5. Print the result.
+
+fn main(){
+    let mut score = 22;
+    score = score + 5;
+    println!("Score is {}", score);
+}

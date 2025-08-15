261bd7e2d71503bfaf312bd144fd814d004e12de
Your Name <diwankhushi428@gmail.com>
2025-08-16 00:51:06 +0530

    exercise 2 - shadowing

--- rust_day1/day1_practice/src/exercises.rs
+++ rust_day1/day1_practice/src/exercises.rs
@@ -3,8 +3,20 @@
 //Question:
 //Make a variable score = 10. Increase it by 5. Print the result.
 
-fn main(){
-    let mut score = 22;
-    score = score + 5;
-    println!("Score is {}", score);
+// fn main(){
+//     let mut score = 22;
+//     score = score + 5;
+//     println!("Score is {}", score);
+// }
+
+//Exercise 2 — Shadowing
+
+//Question:
+//Use shadowing to convert a string "42" to an integer and add 8.   
+
+fn main() {
+    let kiki = "42";
+    let kiki: i32 = kiki.parse().unwrap(); 
+    let kiki = kiki + 8; 
+    println!("Kiki is {}", kiki);
 }

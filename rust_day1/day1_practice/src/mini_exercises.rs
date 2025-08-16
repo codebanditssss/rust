23cb5919e879946c9ad1621fea81cc8530bdf97d
Your Name <diwankhushi428@gmail.com>
2025-08-16 14:15:58 +0530

    exercise 2 - two favorite numbers, do arithmetic

--- rust_day1/day1_practice/src/mini_exercises.rs
+++ rust_day1/day1_practice/src/mini_exercises.rs
@@ -1,17 +1,46 @@
 //Exercise 1 — Convert height to cm and calculate BMI
 
+// use std::io;
+
+// fn main() {
+//     let mut height = String::new();
+//     println!("enter ur height in meters");
+//     io::stdin().read_line(&mut height).expect("failed to read line");
+//     let height: f32 = height.trim().parse().unwrap();
+
+//     let heightcm = height * 100.0;
+//     let weight = 44.0;
+//     let bmi = weight / (height * height);
+
+//     println!("ur height in cm is: {}", heightcm);
+//     println!("ur BMI is: {}", bmi);
+// }
+
+// Exercise 2 — Two favorite numbers, do arithmetic
+
 use std::io;
 
 fn main() {
-    let mut height = String::new();
-    println!("enter ur height in meters");
-    io::stdin().read_line(&mut height).expect("failed to read line");
-    let height: f32 = height.trim().parse().unwrap();
+    let mut kiki = String::new();
+    let mut aash = String::new();
 
-    let heightcm = height * 100.0;
-    let weight = 44.0;
-    let bmi = weight / (height * height);
+    println!("enter ur first fav number");
+    io::stdin().read_line(&mut kiki).expect("failed to read line");
 
-    println!("ur height in cm is: {}", heightcm);
-    println!("ur BMI is: {}", bmi);
+    println!("enter ur second fav number");
+    io::stdin().read_line(&mut aash).expect("failed to read line");
+
+    let kiki: i32 = kiki.trim().parse().unwrap();
+    let aash: i32 = aash.trim().parse().unwrap();   
+
+    let sum = kiki + aash;
+    let diff = kiki - aash;
+    let prod = kiki * aash;
+    let div = kiki / aash;
+    
+    println!("sum: {}", sum);
+    println!("difference: {}", diff);
+    println!("product: {}", prod);
+    println!("division: {}", div);
+
 }

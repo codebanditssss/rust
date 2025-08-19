6b566a1ae8f28e591d8e47e700dfb7ef70d4d36e
Your Name <diwankhushi428@gmail.com>
2025-08-20 00:03:23 +0530

    mutable references (&mut)

--- rust_day2/string_tinkerer_game/src/fun2.rs
+++ rust_day2/string_tinkerer_game/src/fun2.rs
@@ -23,17 +23,38 @@
 
 // Instead of transferring ownership, you can borrow the value:
 
-fn borrow_string(s: &String) {
-    println!("Inside function: {}", s);
-}
+// fn borrow_string(s: &String) {
+//     println!("Inside function: {}", s);
+// }
 
-fn main() {
-    let name = String::from("Rustacean");
-    borrow_string(&name);  
-    println!("{}", name);   // Works fine, still usable
-}
+// fn main() {
+//     let name = String::from("Rustacean");
+//     borrow_string(&name);  
+//     println!("{}", name);   // Works fine, still usable
+// }
 
 // Explanation:
 // &String → means “reference to String”.
 // Function doesn’t take ownership, just borrows it.
 // Caller keeps ownership after the function call.
+
+
+
+
+
+//Mutable References (&mut)
+
+//Sometimes you want to modify the value inside a function. For that, you need a mutable borrow.
+
+fn add_exclamation(s: &mut String) {
+    s.push_str("!");
+}
+
+fn main() {
+    let mut greeting = String::from("Hello");
+    add_exclamation(&mut greeting);
+    println!("{}", greeting);  // Hello!
+}
+
+// Only one mutable reference at a time allowed (to prevent data races).
+// You need both the variable and the reference to be mutable (let mut and &mut).

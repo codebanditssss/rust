8221b0347185d6360768d7743a4126c5072033a4
Your Name <diwankhushi428@gmail.com>
2025-08-19 22:53:41 +0530

    function with parameters (but no return)

--- rust_day2/string_tinkerer_game/src/functions.rs
+++ rust_day2/string_tinkerer_game/src/functions.rs
@@ -18,15 +18,32 @@
 // -> ReturnType → only needed if the function returns something.
 // Function body → curly braces { }.
 
+// Example 1: Function with No Parameters & No Return
 
-fn greet() {
-    println!("hello from a function!");
-}
 
-fn main() {
-    greet(); // function call
-}
+// fn greet() {
+//     println!("hello from a function!");
+// }
+
+// fn main() {
+//     greet(); // function call
+// }
 
 // fn greet() → takes no arguments.
 // Inside main, greet(); calls it.
 // Rust programs always start at main(), so other functions must be called from there.
+
+
+// Example 2: Function with Parameters (but No Return)
+
+fn print_number(num: i32) {
+    println!("The number is: {}", num);
+}
+
+fn main() {
+    print_number(22);
+}
+
+// num: i32 means this function expects an integer parameter.
+// Rust is strictly typed, so you must specify parameter type.
+// Calling without passing the correct type → compiler error.

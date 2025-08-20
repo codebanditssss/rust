d602b98dc0af6c58afdbe3c3a096b1d567139c1d
Your Name <diwankhushi428@gmail.com>
2025-08-20 10:15:57 +0530

    tuple structs in rust

--- rust_day2/day2_practice/src/tempCodeRunnerFile.rs
+++ rust_day2/day2_practice/src/tempCodeRunnerFile.rs
@@ -1,4 +1,5 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+fn main() {
+//     let rect1 = Rectangle { width: 30, height: 50 };
+
+//     println!("the rectangle has width {} and height {}", rect1.width, rect1.height);
+// }
